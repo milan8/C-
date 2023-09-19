da1740dce7cec29912923981e91b04de2e27b658
Milan Antony <28830194+milan8@users.noreply.github.com>
2023-09-20 01:42:46 +0530

    Console App Spinner animation sample code

---
+++ ConsoleSpinnerAnimation/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace testConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int waittime = 90;
+            Console.CursorVisible = false;
+            while (true)
+            {
+
+                Console.Write("/");
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                System.Threading.Thread.Sleep(waittime);
+                Console.Write("-");
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                System.Threading.Thread.Sleep(waittime);
+                Console.Write("\\");
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                System.Threading.Thread.Sleep(waittime);
+                Console.Write("|");
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+                System.Threading.Thread.Sleep(waittime);
+            }
+        }
+    }
+}
